343f9f1f482a0814ce3a290fff1135d41b788426
kader amrichat <kader.amrichat@gmail.com>
2018-03-20 19:13:28 +0100

    node app boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node js + express boiler plate
---
+++ app.js
@@ -0,0 +1,28 @@
+var express =     require('express');
+var bodyParser =  require('body-parser');
+var path =        require('path');
+
+var app = express();
+
+// View Engine
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+//set static path for the front dev
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title : 'My main title'
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000 ");
+});
